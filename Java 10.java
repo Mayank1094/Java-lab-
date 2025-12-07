fbbe10e929cb5e574ab0521b32d6340817ffa64f
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-07 14:26:57 +0530

    Add ThreadPriorityDemo class to demonstrate thread priorities

---
+++ Java 10.java
@@ -0,0 +1,13 @@
+class ThreadPriorityDemo extends Thread {
+public void run() {
+System.out.println(getName() + " priority:" + getPriority());
+}
+public static void main(String[] args) {
+ThreadPriorityDemo t1 = new ThreadPriorityDemo();
+ThreadPriorityDemo t2 = new ThreadPriorityDemo();
+t1.setPriority(Thread.MIN_PRIORITY);
+t2.setPriority(Thread.MAX_PRIORITY);
+t1.start();
+t2.start();
+}
+}
