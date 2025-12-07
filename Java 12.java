8a9ca143f61fff930a0e5fdc0ea51c9737212f3e
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-07 14:28:11 +0530

    Create Java 12.java

---
+++ Java 12.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Banner extends Applet {
+public void paint(Graphics g) {
+g.drawString("Welcome to Java Applet", 50, 50);
+}
+}
+/*
+<applet code="Banner" width="300" height="300">
+</applet>
+*/
