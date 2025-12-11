1a3dcf0e2577e385be78267378e4e2c53835d44a
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-11 12:57:45 +0530

    Update Java 18.java

--- Java 18.java
+++ Java 18.java
@@ -1,18 +1,19 @@
 import java.applet.*;
 import java.awt.*;
 import java.util.*;
-public class TokenApplet extends Applet {
-public void paint(Graphics g) {
-String s = "Java is simple";
-StringTokenizer st = new StringTokenizer(s);
-int y = 20;
-while(st.hasMoreTokens()) {
-g.drawString(st.nextToken(), 20, y);
-y += 20;
+
+public class TokenApplet extends Applet
+{
+public void paint(Graphics g)
+{
+String s="Java is simple program";
+StringTokenizer st=new StringTokenizer(s);
+int y=20;
+
+while(st.hasMoreTokens())
+{
+g.drawString(st.nextToken(),20,y);
+y+=20;
 }
 }
 }
-/*
-<applet code="TokenApplet" width="300" height="300">
-</applet>
-*/
