9bf2df778839c4f28b4f7d4e302a080c14803516
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-11 12:56:55 +0530

    Update Java 17.java

--- Java 17.java
+++ Java 17.java
@@ -1,18 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
-public class FontDemo {
-public static void main(String[] a) {
-JFrame f = new JFrame();
-f.setLayout(new java.awt.FlowLayout());
-f.add(make("Plain", Font.PLAIN));
-f.add(make("Bold", Font.BOLD));
-f.add(make("Italic", Font.ITALIC));
-f.setSize(250, 150);
+class FontDemo
+{
+public static void main(String[] a)
+{
+JFrame f=new JFrame();
+f.setLayout(new FlowLayout());
+f.add(make("This is Arial Font",Font.PLAIN));
+f.add(make("This is Times New Roman Font",Font.BOLD));
+f.add(make("This is Courier New Font",Font.ITALIC));
+f.setSize(250,150);
 f.setVisible(true);
 }
-static JLabel make(String s, int t) {
-JLabel l = new JLabel(s);
-l.setFont(new Font("Serif", t, 18));
+static JLabel make(String s,int t)
+{
+JLabel l=new JLabel(s);
+l.setFont(new Font("Serif",t,18));
 return l;
 }
 }
