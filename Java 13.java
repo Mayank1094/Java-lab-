5068e52ad2ff44e1620939f0184e3afce636ffc5
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-11 12:31:35 +0530

    Update Java 13.java

--- Java 13.java
+++ Java 13.java
@@ -1,21 +1,21 @@
-public class CountArgs {
-public static void main(String[] args) {
-int ints=0, floats=0, strings=0;
-for(String a : args) {
-try {
+class CountArgs{
+public static void main(String[] args){
+int ints=0,floats=0,strings=0;
+for(String a:args){
+try{
 Integer.parseInt(a);
 ints++;
-} catch(NumberFormatException e1) {
-try {
+}catch(NumberFormatException e1){
+try{
 Float.parseFloat(a);
 floats++;
-} catch(NumberFormatException e2) {
+}catch(NumberFormatException e2){
 strings++;
 }
 }
 }
-System.out.println("Integers: " + ints);
-System.out.println("Floats: " + floats);
-System.out.println("Strings: " + strings);
+System.out.println("Integers: "+ints);
+System.out.println("Floats: "+floats);
+System.out.println("Strings: "+strings);
 }
 }
