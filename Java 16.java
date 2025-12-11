0fde008e5228cde799d85126f68d31f020d7dc52
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-11 12:47:39 +0530

    Update Java 16.java

--- Java 16.java
+++ Java 16.java
@@ -1,23 +1,21 @@
-import java.applet.Applet;
-import java.awt.*;
+import javax.swing.*;
 import java.awt.event.*;
-public class EventDemo extends Applet implements ActionListener {
-Button b;
-String message = "Hello";
-public void init() {
-b = new Button("Click");
-add(b);
-b.addActionListener(this);
+public class A
+{
+public static void main(String[] args)
+{
+JFrame f=new JFrame();
+JButton b=new JButton("Click me");
+b.setBounds(50,50,120,30);
+b.addActionListener(e->System.out.println("Button Clicked"));
+b.addMouseListener(new MouseAdapter(){
+public void mouseEntered(MouseEvent e){
+System.out.println("Mouse Entered!");
 }
-public void actionPerformed(ActionEvent e) {
-message = "Button was clicked!";
-repaint();
+});
+f.add(b);
+f.setSize(250,200);
+f.setLayout(null);
+f.setVisible(true);
 }
-public void paint(Graphics g) {
-g.drawString(message, 50, 100);
 }
-}
-/*
-<applet code="EventDemo" width="200" height="200">
-</applet>
-*/
