a19355e94af833a0d0b8a5ce0bec7a9feff72958
Mayank1094 <mayankkamble1094@gmail.com>
2025-12-11 12:18:58 +0530

    Update Java 11.java

--- Java 11.java
+++ Java 11.java
@@ -1,15 +1,15 @@
 import java.applet.Applet;
 import java.awt.*;
-public class Face extends Applet {
-public void paint(Graphics g) {
-g.drawOval(100, 50, 100, 100);
-g.fillOval(125, 80, 10, 10);
-g.fillOval(165, 80, 10, 10);
-g.drawLine(150, 90, 150, 110);
-g.drawArc(130, 110, 40, 20, 0, -180);
+public class Faces extends Applet{
+public void paint(Graphics g){
+g.drawOval(100,50,100,100);
+g.fillOval(125,80,10,10);
+g.fillOval(165,80,10,10);
+g.drawLine(150,90,150,110);
+g.drawArc(130,110,40,20,0,-180);
 }
 }
-/*
-<applet code="Face" width="300" height="300">
-</applet>
-*/
+
+<html>
+<applet code="Faces.class" width="300" height="300"></applet>
+</html>
